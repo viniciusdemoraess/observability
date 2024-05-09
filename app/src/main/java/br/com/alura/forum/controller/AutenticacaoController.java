6568c7d96eca281625549ee59630200ea530a724
viniciusdemoraess <vinicius.moraes@id-cotton.com>
2024-05-09 00:17:54 -0400

    feat: config metrics authUserSuccess and authUserErrors

--- app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -2,6 +2,8 @@ package br.com.alura.forum.controller;
 
 import javax.validation.Valid;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +24,22 @@ import br.com.alura.forum.controller.form.LoginForm;
 @RequestMapping("/auth")
 @Profile(value = {"prod", "test"})
 public class AutenticacaoController {
-	
+
+	Counter authUserSuccess;
+
+	Counter authUserErrors;
+
+	public AutenticacaoController(MeterRegistry registry) {
+		authUserSuccess = Counter.builder("auth_user_success")
+				.description("usuarios autenticados")
+				.register(registry);
+
+		authUserErrors = Counter.builder("auth_user_error")
+				.description("erros de login")
+				.register(registry);
+	}
+
+
 	@Autowired
 	private AuthenticationManager authManager;
 	
@@ -35,10 +52,12 @@ public class AutenticacaoController {
 		
 		try {
 			Authentication authentication = authManager.authenticate(dadosLogin);
-			String token = tokenService.gerarToken(authentication); 		
+			String token = tokenService.gerarToken(authentication);
+			authUserSuccess.increment();
 			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
 			
 		} catch (AuthenticationException e) {
+			authUserErrors.increment();
 			return ResponseEntity.badRequest().build();
 		}
 
