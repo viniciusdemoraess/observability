8e99485765a44bbfdf92251b5eb029cdf913be1c
viniciusdemoraess <vinicius.moraes@id-cotton.com>
2024-05-09 00:24:08 -0400

    feat: added configs to execute docker-compse with app and prometheus

--- app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,9 +1,6 @@
 package br.com.alura.forum.controller;
 
 import javax.validation.Valid;
-
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
@@ -15,31 +12,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @RestController
 @RequestMapping("/auth")
 @Profile(value = {"prod", "test"})
 public class AutenticacaoController {
-
+	
 	Counter authUserSuccess;
-
 	Counter authUserErrors;
 
 	public AutenticacaoController(MeterRegistry registry) {
-		authUserSuccess = Counter.builder("auth_user_success")
-				.description("usuarios autenticados")
-				.register(registry);
-
-		authUserErrors = Counter.builder("auth_user_error")
-				.description("erros de login")
-				.register(registry);
-	}
-
-
+    	authUserSuccess = Counter.builder("auth_user_success")
+            .description("usuarios autenticados")
+            .register(registry);
+    	
+    	authUserErrors = Counter.builder("auth_user_error")
+                .description("erros de login")
+                .register(registry);
+    }
+    
 	@Autowired
 	private AuthenticationManager authManager;
 	
@@ -59,8 +55,6 @@ public class AutenticacaoController {
 		} catch (AuthenticationException e) {
 			authUserErrors.increment();
 			return ResponseEntity.badRequest().build();
-		}
-
-		
+		}	
 	}
 }
